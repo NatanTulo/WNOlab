73f4ea7f3f1ab4eec7636f7fef18ab9014ca1cd7
NatanTulo <135972515+NatanTulo@users.noreply.github.com>
2024-12-08 18:43:34 +0100

    obrót trójkąta wokół punktu x2

--- zad3.py
+++ zad3.py
@@ -30,8 +30,8 @@ ax.set_aspect('equal')
 # Elementy graficzne
 ax.plot(x_vals, y_vals, 'g-', lw=0.5)  # Funkcja bazowa
 point1, = ax.plot([], [], 'ro')  # wierzchołek trójkąta
-point2, = ax.plot([], [], 'ro')  # wierzchołek trójkąta
-point3, = ax.plot([], [], 'ro')  # wierzchołek trójkąta
+point2, = ax.plot([], [], 'go')  # wierzchołek trójkąta
+point3, = ax.plot([], [], 'bo')  # wierzchołek trójkąta
 side1, = ax.plot([], [], 'b-', lw=1)  # Bok trójkąta
 side2, = ax.plot([], [], 'b-', lw=1)  # Bok trójkąta
 side3, = ax.plot([], [], 'b-', lw=1)  # Bok trójkąta
@@ -43,43 +43,45 @@ x_curve, y_curve = [], []
 
 def init():
     x1, y1 = first_point_x, f(first_point_x)
+    x1, y1, x2, y2, x3, y3 = find_triangle(first_point_x, a, x_vals, y_vals)
+    
     point1.set_data([x1], [y1])
-    point2.set_data([], [])
-    point3.set_data([], [])
-    side1.set_data([], [])
-    side2.set_data([], [])
-    side3.set_data([], [])
+    point2.set_data([x2], [y2])
+    # point3.set_data([x3], [y3])
+    side1.set_data([x1, x2], [y1, y2])
+    side2.set_data([x2, x3], [y2, y3])
+    side3.set_data([x3, x1], [y3, y1])
     radius_line.set_data([], [])
     curve.set_data([], [])
-    return point1, point2, point3, radius_line, curve
+    return point1, side1, side2, side3, radius_line, curve, point2#, point3
 
 def find_next_x(x1, y1, a, x_vals, y_vals):
-    distances = np.sqrt((x_vals - x1)**2 + (y_vals - y1)**2)
-    closest_idx = np.abs(distances - a).argmin()
-    return x_vals[closest_idx]
-
-def update(frame):
-    global first_point_x
-    if frame == 0:
-        x_curve.clear()
-        y_curve.clear()
+    # Znajdujemy tylko punkty na prawo od x1
+    mask = x_vals > x1
+    x_vals_right = x_vals[mask]
+    y_vals_right = y_vals[mask]
     
+    # Obliczamy odległości tylko dla punktów po prawej stronie
+    distances = np.sqrt((x_vals_right - x1)**2 + (y_vals_right - y1)**2)
+    closest_idx = np.abs(distances - a).argmin()
+    
+    return x_vals_right[closest_idx]
+
+def find_triangle(x1, a, x_vals, y_vals):
     # Aktualizacja pierwszego punktu
-    x1, y1 = first_point_x, f(first_point_x)
-    point1.set_data([x1], [y1])
+    y1 = f(x1)
     
     # Znalezienie drugiego punktu w odległości 'a' w linii prostej
     x2 = find_next_x(x1, y1, a, x_vals, y_vals)
     y2 = f(x2)
-    # point2.set_data([x2], [y2])
 
     # Obliczenie wektora kierunkowego prostej point1-point2
     dx = x2 - x1
     dy = y2 - y1
     
     # Obrót wektora o 90 stopni (prostopadły)
-    perpendicular_dx = -dy
-    perpendicular_dy = dx
+    perpendicular_dx = dy  
+    perpendicular_dy = -dx 
     
     # Normalizacja wektora prostopadłego
     length = np.sqrt(perpendicular_dx**2 + perpendicular_dy**2)
@@ -87,21 +89,62 @@ def update(frame):
     perpendicular_dy /= length
     
     # Wyznaczenie punktu na prostej prostopadłej
-    x3 = (x1+x2)/2 - perpendicular_dx * a*np.sqrt(3)/2
-    y3 = (y1+y2)/2 - perpendicular_dy * a*np.sqrt(3)/2
-    # point3.set_data([x3], [y3])
+    x3 = (x1+x2)/2 + perpendicular_dx * a*np.sqrt(3)/2 
+    y3 = (y1+y2)/2 + perpendicular_dy * a*np.sqrt(3)/2
+    
+    # Sprawdzenie czy punkt jest poniżej funkcji i odbicie jeśli tak
+    y3_function = f(x3)
+    if y3 < y3_function:  # jeśli punkt jest poniżej funkcji
+        # Odbijamy punkt względem prostej point1-point2
+        x3 = (x1+x2)/2 - perpendicular_dx * a*np.sqrt(3)/2
+        y3 = (y1+y2)/2 - perpendicular_dy * a*np.sqrt(3)/2
+    
+    return x1, y1, x2, y2, x3, y3
 
-    # Rysowanie linii prostopadłej
-    # radius_line.set_data([x3, x4], [y3, y4])
+def rotate_point(x, y, cx, cy, angle):
+    # Przesunięcie punktu względem środka obrotu
+    translated_x = x - cx
+    translated_y = y - cy
+    
+    # Obrót
+    cos_a = np.cos(angle)
+    sin_a = np.sin(angle)
+    rotated_x = translated_x * cos_a - translated_y * sin_a
+    rotated_y = translated_x * sin_a + translated_y * cos_a
+    
+    return rotated_x + cx, rotated_y + cy
+
+def update(frame):
+    global first_point_x
+    if frame == 0:
+        x_curve.clear()
+        y_curve.clear()
+    
+    # Znajdujemy pierwotne położenie trójkąta
+    x1, y1, x2, y2, x3, y3 = find_triangle(first_point_x, a, x_vals, y_vals)
+    
+    # Obliczamy kąt obrotu (frame określa postęp animacji)
+    angle = -(2 * np.pi * frame) / steps
+    
+    # Obracamy punkty 1 i 3 wokół punktu 2
+    x1, y1 = rotate_point(x1, y1, x2, y2, angle)
+    x3, y3 = rotate_point(x3, y3, x2, y2, angle)
     
     # Rysowanie linii między punktami
     side1.set_data([x1, x2], [y1, y2])
     side2.set_data([x2, x3], [y2, y3])
     side3.set_data([x3, x1], [y3, y1])
-    # print(np.sqrt((x1-x2)**2 + (y1-y2)**2), np.sqrt((x2-x3)**2 + (y2-y3)**2), np.sqrt((x3-x1)**2 + (y3-y1)**2))
-    # first_point_x = x2
     
-    return point1, point2, point3, radius_line, curve
+    point1.set_data([x1], [y1])
+    point2.set_data([x2], [y2])
+    # point3.set_data([x3], [y3])
+    
+    x_curve.append(x1)
+    y_curve.append(y1)
+    curve.set_data(x_curve, y_curve)
+    print(x1,x2)
+    
+    return point1, side1, side2, side3, radius_line, curve , point2#, point3
 
 ani = FuncAnimation(fig, update, frames=steps, init_func=init, interval=interval, blit=True)
 plt.show()

a38b961b2226d111661b75dac0af8a36fe4b5cb4
NatanTulo <s193527@student.pg.edu.pl>
2024-12-09 12:26:40 +0100

    trójkąt o złej długości przyprostokątnej przy ogonie

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.alpha = np.random.uniform(0, 360)  # kierunek w stopniach
+        self.alpha = 90 #np.random.uniform(0, 360)  # kierunek w stopniach
+        self.beta = 90 #np.random.uniform(0, 90)    # kąt rozwarcia w stopniach
+        self.a = np.random.uniform(0, 10)       # długość wektora
+        self.b = 2                              # długość przeciwnego wektora
 
     def __getitem__(self, index):
         if index == 0:
@@ -56,19 +59,69 @@ def graham_scan(points):
     return hull
 
 def plot_points_and_hull(points, hull):
-    # Rozdzielamy x i y dla wszystkich punktów
     x_coords = [p.x for p in points]
     y_coords = [p.y for p in points]
     plt.scatter(x_coords, y_coords, color='blue', label='Points')
+    plt.axis('equal')
     
-    # Rysowanie kierunków dla każdego punktu
+    # Rysowanie kątów dla każdego punktu
     for point in points:
-        angle_rad = np.radians(point.alpha)
-        dx = np.cos(angle_rad) * 5  # długość strzałki
-        dy = np.sin(angle_rad) * 5
-        plt.arrow(point.x, point.y, dx, dy, 
-                 head_width=0.5, head_length=1, fc='green', ec='green')
-    
+        alpha_rad = np.radians(point.alpha)
+        beta_rad = np.radians(point.beta)
+        
+        # Obliczanie kątów dla obu ramion
+        angle1 = alpha_rad - beta_rad/2
+        angle2 = alpha_rad + beta_rad/2
+        
+        # Rysowanie pierwszego ramienia
+        dx1 = np.cos(angle1) * point.a
+        dy1 = np.sin(angle1) * point.a
+        plt.plot([point.x, point.x + dx1], [point.y, point.y + dy1], color='green')
+        
+        # Rysowanie drugiego ramienia
+        dx2 = np.cos(angle2) * point.a
+        dy2 = np.sin(angle2) * point.a
+        plt.plot([point.x, point.x + dx2], [point.y, point.y + dy2], color='green')
+        
+        # Rysowanie dwusiecznej (alpha)
+        dx_alpha = np.cos(alpha_rad) * point.a
+        dy_alpha = np.sin(alpha_rad) * point.a
+        plt.plot([point.x, point.x + dx_alpha], [point.y, point.y + dy_alpha], color='red')
+        
+        # Rysowanie przeciwnego wektora o długości b
+        dx_opposite = -np.cos(alpha_rad) * point.b
+        dy_opposite = -np.sin(alpha_rad) * point.b
+        tail_end_x = point.x + dx_opposite
+        tail_end_y = point.y + dy_opposite
+        plt.plot([point.x, tail_end_x], [point.y, tail_end_y], color='blue')
+        
+        # Rysowanie poprawnego trójkąta prostokątnego w miejscu przeciwnym
+        left_angle = alpha_rad + np.pi + beta_rad/2
+        right_angle = alpha_rad + np.pi - beta_rad/2
+
+        # Obliczanie punktów końcowych ogona
+        tail_end_x = point.x + dx_opposite
+        tail_end_y = point.y + dy_opposite
+
+        # Punkty lewego i prawego ramienia
+        left_end_x = point.x + np.cos(left_angle) * point.b
+        left_end_y = point.y + np.sin(left_angle) * point.b
+
+        right_end_x = point.x + np.cos(right_angle) * point.b
+        right_end_y = point.y + np.sin(right_angle) * point.b
+
+        # Tworzenie poprawnego trójkąta prostokątnego
+        triangle_x = [point.x, left_end_x, right_end_x]
+        triangle_y = [point.y, left_end_y, right_end_y]
+
+        plt.fill(triangle_x, triangle_y, alpha=0.5, fc='blue', ec='black')
+
+
+    # Rysowanie trójkąta prostokątnego
+    triangle_x = [point.x, tail_end_x, right_end_x, left_end_x]
+    triangle_y = [point.y, tail_end_y, right_end_y, left_end_y]
+    plt.fill(triangle_x, triangle_y, alpha=0.2, fc='yellow', ec='orange')
+
     # Rysowanie otoczki wypukłej
     hull_with_closure = hull + [hull[0]]
     hull_x = [p.x for p in hull_with_closure]
